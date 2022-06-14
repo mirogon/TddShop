125f032a736c6cd94f479201061fe70e07d7a9bf
mirogon <m1smr@hotmail.com>
2022-06-14 16:08:45 +0200

    ItemBatch

---
+++ CSharp/Shop/Items/ItemBatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Items {
+    public class ItemBatch {
+        private Item item;
+        private int stock;
+        public ItemBatch(Item item, int stock) {
+            this.item = item;
+            this.stock = stock;
+        }
+        public Item Item {
+            get { return item; }
+        }
+        public int Stock {
+            get { return stock; }
+        }
+    }
+}
---
+++ CSharp/ShopTest/Items/ItemBatchTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopTest.Items {
+    using Shop.Items;
+    public class ItemBatchTest {
+        [Fact]
+        public void ItemAndStock_EqualsGiven() {
+            Item i = new Item("Blue Shoes", 50);
+            ItemBatch batch = new ItemBatch(i, 100);
+
+            Assert.Equal(i, batch.Item);
+            Assert.Equal(100, batch.Stock);
+        }
+    }
+}
