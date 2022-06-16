644d7b49fa23d26cecf28b466ef6284e28fa1286
mirogon <m1smr@hotmail.com>
2022-06-16 02:09:54 +0200

    Structure notes

--- CSharp/Shop/App/App.cs
+++ CSharp/Shop/App/App.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace Shop.App {
     using Customers;
+    using Ui;
     public class App {
 
-        public App(Customer customer, Shop shop) {
+        private Ui ui;
+        public App(Ui ui, Customer customer, Shop shop) {
+            this.ui = ui;
+        }
+        public void Start() {
+            ui.MainMenu();
         }
     }
 }
--- CSharp/ShopTest/App/AppTest.cs
+++ CSharp/ShopTest/App/AppTest.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
 
 namespace ShopTest.App {
     using Shop.Customers;
     using Shop;
     using Shop.App;
+    using Shop.Ui;
     public class AppTest {
         [Fact]
-        public void Create() {
+        public void Start_ShowsMainMenu() {
             Customer customer = new Customer(1000);
             Shop shop = new Shop();
-            App app = new App(customer, shop);
+            var uiMock = new Mock<Ui>();
+            uiMock.Setup(ui => ui.MainMenu());
+            App app = new App(uiMock.Object, customer, shop);
+
+            app.Start();
+
+            uiMock.Verify(ui => ui.MainMenu());
         }
     }
 }
