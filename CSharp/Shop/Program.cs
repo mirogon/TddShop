fd1dc7a23e95348dc0056dd2b61161d5416e48f2
mirogon <m1smr@hotmail.com>
2022-06-21 06:09:43 +0200

    Main Menu test

--- CSharp/Shop/Program.cs
+++ CSharp/Shop/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shop.App;
+using Shop.Ui;
+using Shop.Ui.CLI;
+using Shop.Customers;
+using Shop;
+using Shop.Items;
+
+//UI
+UiTextCreator uiTextCreator = new ConsoleUiTextCreator();
+ConsoleInput consoleInput = new StandardConsoleInput();
+Ui ui = new ConsoleUi(uiTextCreator, consoleInput);
+
+//Customer
+Customer customer = new Customer(1000);
+
+//Shop
+Shop.Shop shop = new Shop.Shop();
+Item redShoes = new Item("Red Shoes", 55);
+ItemBatch batch = new ItemBatch(redShoes, 10);
+
+App app = new App(ui, customer, shop);
+app.Start();
---
+++ CSharp/Shop/Ui/CLI/StandardConsoleInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Ui.CLI {
+    public class StandardConsoleInput : ConsoleInput{
+        public string ReadLine() {
+            return Console.ReadLine();
+        }
+    }
+}
