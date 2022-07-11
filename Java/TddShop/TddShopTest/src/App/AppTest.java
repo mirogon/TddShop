c6ba4762cfe29d4016635459ae3094320e9ee348
Marcel Römer <m1smr@hotmail.com>
2022-07-11 16:11:39 +0200

    more App tests

--- Java/TddShop/TddShopTest/src/App/AppTest.java
+++ Java/TddShop/TddShopTest/src/App/AppTest.java
@@ -3,7 +3,8 @@ package App;
 import Item.*;
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import Customer.*;
@@ -42,6 +43,103 @@ public class AppTest {
 
         verify(uiMock).MainMenu();
         verify(uiMock).ShopMenu(shop.Items());
+    }
+    @Test
+    public void MainMenu_WithCustomerInput_CallsCustomerMenu(){
+        Customer c = new Customer(new Wallet(1000));
+        Shop shop = new Shop();
+        Ui uiMock = mock(Ui.class);
+
+        when(uiMock.MainMenu()).thenReturn("Customer");
+
+        App app = new App(uiMock, c, shop);
+
+        app.Update();
+        app.Update();
+
+        verify(uiMock).MainMenu();
+        verify(uiMock).CustomerMenu(c);
+    }
+    @Test
+    public void MainMenu_WithExitInput_Exits(){
+        Customer c = new Customer(new Wallet(1000));
+        Shop shop = new Shop();
+        Ui uiMock = mock(Ui.class);
+
+        when(uiMock.MainMenu()).thenReturn("Exit");
+
+        App app = new App(uiMock, c, shop);
+
+        assertTrue(app.Running());
+
+        app.Update();
+
+        assertFalse(app.Running());
+
+        verify(uiMock).MainMenu();
+    }
+    @Test
+    public void ShopMenu_Back_ReturnsToMainMenu(){
+        Customer customer = new Customer(new Wallet(1000));
+        Shop shop = new Shop();
+        Ui uiMock = mock(Ui.class);
+
+        when(uiMock.MainMenu()).thenReturn("Shop");
+        when(uiMock.ShopMenu(any())).thenReturn("Back");
+
+        App app = new App(uiMock, customer, shop);
+
+        Item item = new Item("TestItem", 150);
+        ItemBatch itemBatch = new ItemBatch(item, 1);
+        shop.Add(itemBatch);
+
+        app.Update();
+        app.Update();
+
+        verify(uiMock).MainMenu();
+        verify(uiMock).ShopMenu(shop.Items());
+
+        app.Update();
+        verify(uiMock, times(2)).MainMenu();
+    }
+    @Test
+    public void CustomerMenu_WithBackInput_ReturnsToMainMenu(){
+        Customer customer = new Customer(new Wallet(1000));
+        Shop shop = new Shop();
+        Ui uiMock = mock(Ui.class);
+
+        when(uiMock.MainMenu()).thenReturn("Customer");
+        when(uiMock.CustomerMenu(any())).thenReturn("Back");
+
+        App app = new App(uiMock, customer, shop);
+
+        app.Update();
+        app.Update();
+
+        verify(uiMock).MainMenu();
+        verify(uiMock).CustomerMenu(customer);
+
+        app.Update();
+
+        verify(uiMock, times(2)).MainMenu();
+    }
+    @Test
+    public void CustomerMenu_WithBuyInput_GoesToCustomerBuyMenu(){
+        Customer customer = new Customer(new Wallet(1000));
+        Shop shop = new Shop();
+        Ui uiMock = mock(Ui.class);
+
+        when(uiMock.MainMenu()).thenReturn("Customer");
+        when(uiMock.CustomerMenu(any())).thenReturn("Buy");
+
+        App app = new App(uiMock, customer, shop);
+
+        app.Update();
+        verify(uiMock).MainMenu();
+        app.Update();
+        verify(uiMock).CustomerMenu(customer);
+        app.Update();
+        verify(uiMock).CustomerBuyMenu(any());
     }
 
 }
--- Java/TddShop/src/App/App.java
+++ Java/TddShop/src/App/App.java
@@ -7,7 +7,9 @@ import Shop.*;
 enum AppState{
     Exit,
     MainMenu,
-    ShopMenu
+    ShopMenu,
+    CustomerMenu,
+    CustomerBuyMenu
 }
 
 public class App {
@@ -17,12 +19,19 @@ public class App {
 
     AppState appState;
 
+    boolean running;
+
     public App(Ui ui, Customer c, Shop s){
         this.ui = ui;
         this.customer = c;
         this.shop = s;
+        running = true;
 
         appState = AppState.MainMenu;
+    }
+
+    public boolean Running(){
+        return running;
     }
     public void Update(){
 
@@ -32,9 +41,31 @@ public class App {
             if(input == "Shop"){
                 appState = AppState.ShopMenu;
             }
+            if(input == "Customer"){
+               appState = AppState.CustomerMenu;
+            }
+            if(input == "Exit"){
+                appState = AppState.Exit;
+                running = false;
+            }
         }
         else if(appState == AppState.ShopMenu){
             input = ui.ShopMenu(shop.Items());
+            if(input == "Back"){
+                appState = AppState.MainMenu;
+            }
+        }
+        else if(appState == AppState.CustomerMenu){
+            input = ui.CustomerMenu(customer);
+            if(input == "Back"){
+                appState = AppState.MainMenu;
+            }
+            else if(input =="Buy"){
+                appState = AppState.CustomerBuyMenu;
+            }
+        }
+        else if(appState == AppState.CustomerBuyMenu){
+            input = ui.CustomerBuyMenu(shop.Items());
         }
     }
 }
